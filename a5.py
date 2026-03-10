dfbb176424257fac0d6ae0d3bc3716e628dc4767
vaishnavi5-tech <mvaishnavi643@gmail.com>
2026-03-10 10:32:22 +0530

    Add files via upload

---
+++ a5.py
@@ -0,0 +1,47 @@
+import random
+def display(room):
+    print(room)
+
+
+room = [
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+]
+print("All the rooms are dirty")
+display(room)
+
+x = 0  
+y = 0  
+
+while x < 4:  
+    while y < 4:  
+        
+        room[x][y] = random.choice([0, 1])
+        y += 1 
+    x += 1  
+    y = 0  
+
+print("Before cleaning the room, I detect all of these random dirts")
+display(room)
+
+x = 0  
+y = 0  
+z = 0  
+while x < 4:  
+    while y < 4:  
+        if room[x][y] == 1: 
+            print("Vacuum in this location now:", x, y)
+            room[x][y] = 0  
+            print("Cleaned:", x, y)
+            z += 1  
+        y += 1  
+    x += 1  
+    y = 0  
+
+
+performance = (100 - ((z / 16) * 100))
+print("Room is clean now. Thanks for using the Robot Vacuum Cleaner!")
+display(room)
+print('Performance=', performance, '%')
